f7024bad40ce719a044a8b068f07094e8619cef4
Pj Mara <funpjm@gmail.com>
2016-11-18 13:09:25 -0500

    removed move y thingie

--- feetCad.groovy
+++ feetCad.groovy
@@ -79,7 +79,7 @@ class Feet implements ICadGenerator, IParameterChanged{
 		CSG connectorRectangle = new Cube (10, shaftmap.get("hornBaseDiameter") * 2.5, hornOffset * 3).toCSG()
 
 		CSG connector = connectorBase.union(connectorRectangle)
-		connector = connector.rotz(90).movey(110)
+		connector = connector.rotz(90)
 
 		connector = defaultCadGen.moveDHValues(connector,dh)
 		defaultCadGen.add(allCad,connector,dh.getListener())
