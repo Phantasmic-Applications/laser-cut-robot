496fdb230cf15ad3cd5988ced84731ced5f1811a
Pj Mara <funpjm@gmail.com>
2016-11-30 14:07:58 -0500

    cool or no

--- feetCad.groovy
+++ feetCad.groovy
@@ -75,7 +75,7 @@ class Feet implements ICadGenerator, IParameterChanged{
 
 		
 
-			CSG topKey = new Cube (shaftmap.get("hornBaseDiameter"),dh.getR() , hornOffset * 5).toCSG()
+			CSG topKey = new Cube (shaftmap.get("hornBaseDiameter"),dh.getR() , hornOffset * 5).toCSG().toZMin().toYMin()
 			//.movey(shaftmap.get("hornBaseDiameter")*2)
 			topKey = defaultCadGen.moveDHValues(topKey,dh)
 			defaultCadGen.add(allCad, topKey, dh.getListener())
